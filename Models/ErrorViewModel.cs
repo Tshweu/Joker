13f9ef8101b0ac192972fdb9c2f5fa427e0d03a6
Tshweu Sephiri <58332236+Tshweu@users.noreply.github.com>
2022-09-23 13:00:17 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Joker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
